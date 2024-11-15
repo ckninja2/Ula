58f5b948a03a5dfe576b2a400b2148e2f848e004
ckninja2 <123234243+ckninja2@users.noreply.github.com>
2024-11-15 12:28:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import urllib.request
 
 app = Flask(__name__)
 
+def get_google_time():
+    request = urllib.request.Request("http://www.google.com", method="HEAD")
+    with urllib.request.urlopen(request) as response:
+        date_str = response.headers['Date']
+    return date_str
+    
 @app.route('/')
 def hello():
-    return "Hello, World! Welcome to Koyeb!"
+    return f"Hello, World! Welcome to Koyeb!\n{get_google_time()}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
