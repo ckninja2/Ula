dac08acf1f581062bb65186ae8ab6ea7b2a978b1
ckninja2 <123234243+ckninja2@users.noreply.github.com>
2024-11-17 10:17:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import urllib.request
 
 app = Flask(__name__)
@@ -11,7 +11,8 @@ def get_google_time():
     
 @app.route('/')
 def hello():
-    return f"Hello, World! Welcome to Koyeb!\n{get_google_time()}"
+    requester_ip = request.remote_addr
+    return f"Hello, World! Welcome to Koyeb!\n\n{get_google_time()}\n\n{requester_ip}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
